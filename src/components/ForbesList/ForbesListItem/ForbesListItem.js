c569b780216e28ee6163e68e830ad4e7a4887cb0
Taisiya Tomashevska <taya22011985@gmail.com>
2023-03-11 21:23:24 +0200

    add alt

--- src/components/ForbesList/ForbesListItem/ForbesListItem.js
+++ src/components/ForbesList/ForbesListItem/ForbesListItem.js
@@ -3,7 +3,7 @@ import { AiFillDollarCircle } from "react-icons/ai";
 import css from './ForbesListItem.module.css'
 export const ForbesItem = ({userData: { id, name, avatar, capital, isIncrease } }) => { 
     return (<li className={css.item} key={ id}>
-        <img className={css.avatar} src={avatar} />
+        <img className={css.avatar} src={avatar} alt={name } />
         
         <p className={css.name}> {name}</p>
       <div className={css.wraper}>
